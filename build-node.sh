7649b6780106c8e56eb727b1c2ecff3030abc7e1
Svyatoslav Nikolsky <svyatonik@gmail.com>
2020-07-29 13:47:37 +0300

    fix node build

--- build-node.sh
+++ build-node.sh
@@ -2,4 +2,4 @@
 . ./prelude.sh
 
 cargo build --manifest-path=$BRIDGES_REPO_PATH/bin/node/node/Cargo.toml
-cp $BRIDGES_REPO_PATH/target/release/bridge-node ./bin
+cp $BRIDGES_REPO_PATH/target/debug/bridge-node ./bin
