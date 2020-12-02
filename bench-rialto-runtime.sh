7de308d2bfdb29a215f194789a566cc6b505e687
Svyatoslav Nikolsky <svyatonik@gmail.com>
2020-12-02 16:58:31 +0300

    message lane pallet benchmarks

--- bench-rialto-runtime.sh
+++ bench-rialto-runtime.sh
@@ -4,9 +4,6 @@
 cargo build --manifest-path=$BRIDGES_REPO_PATH/bin/rialto/node/Cargo.toml --release --features runtime-benchmarks
 cp $BRIDGES_REPO_PATH/target/release/rialto-bridge-node ./bin/rialto-bridge-node-benchmarks
 
-RUST_LOG=runtime=trace ./bin/rialto-bridge-node-benchmarks benchmark --pallet bridge-eth-poa --extrinsic import_unsigned_header_best_case
-RUST_LOG=runtime=trace ./bin/rialto-bridge-node-benchmarks benchmark --pallet bridge-currency-exchange --extrinsic import_peer_transaction_best_case
-RUST_LOG=runtime=trace ./bin/rialto-bridge-node-benchmarks benchmark --pallet bridge-currency-exchange --extrinsic import_peer_transaction_when_recipient_does_not_exists
-RUST_LOG=runtime=trace ./bin/rialto-bridge-node-benchmarks benchmark --pallet bridge-currency-exchange --extrinsic import_peer_transaction_when_transaction_size_increases
-RUST_LOG=runtime=trace ./bin/rialto-bridge-node-benchmarks benchmark --pallet bridge-currency-exchange --extrinsic import_peer_transaction_when_proof_size_increases
-RUST_LOG=runtime=trace ./bin/rialto-bridge-node-benchmarks benchmark --pallet bridge-currency-exchange --extrinsic import_peer_transaction_worst_case
+#RUST_LOG=runtime=trace ./bin/rialto-bridge-node-benchmarks benchmark --pallet=pallet_bridge_eth_poa --extrinsic=*
+#RUST_LOG=runtime=trace ./bin/rialto-bridge-node-benchmarks benchmark --pallet=pallet_bridge_currency_exchange --extrinsic=*
+RUST_LOG=runtime=trace ./bin/rialto-bridge-node-benchmarks benchmark --pallet=pallet_message_lane --extrinsic=*
