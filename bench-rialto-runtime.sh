ad6dfed670a60908dfe0be1ec7a7cf14f27d4909
Svyatoslav Nikolsky <svyatonik@gmail.com>
2020-12-16 16:57:57 +0300

    flush

--- bench-rialto-runtime.sh
+++ bench-rialto-runtime.sh
@@ -6,4 +6,4 @@ cp $BRIDGES_REPO_PATH/target/release/rialto-bridge-node ./bin/rialto-bridge-node
 
 #RUST_LOG=runtime=trace ./bin/rialto-bridge-node-benchmarks benchmark --pallet=pallet_bridge_eth_poa --extrinsic=*
 #RUST_LOG=runtime=trace ./bin/rialto-bridge-node-benchmarks benchmark --pallet=pallet_bridge_currency_exchange --extrinsic=*
-RUST_LOG=runtime=trace ./bin/rialto-bridge-node-benchmarks benchmark --pallet=pallet_message_lane --extrinsic=*
+RUST_LOG=runtime=trace,pallet_substrate_bridge=trace,pallet_message_lane=trace,pallet_bridge_call_dispatch=trace ./bin/rialto-bridge-node-benchmarks benchmark --pallet=pallet_message_lane --extrinsic=*
